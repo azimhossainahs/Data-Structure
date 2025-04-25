c95d4396ab4dedd8f849c87c9c569b7b5c06264b
azimhossain <azimhossain.ahs@gmail.com>
2025-04-25 17:48:02 +0600

    Linear search in c code

--- LinearSearch.c
+++ LinearSearch.c
@@ -1,7 +1,27 @@
+//Linear Search
 #include<stdio.h>
 int main(){
-    //Linear Search
-    printf("Hello");
-    
+    int n;
+    printf("Enter (number of element) n : ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d element : ", n);
+    for(int i = 0;i<n;i++){
+        scanf("%d", &arr[i]);
+    }
+
+    int element, j;
+    printf("Element to search  : ");
+    scanf("%d", &element);
+
+    //find the element
+    for(j = 0;j<n;j++){
+        if(element == arr[j]){
+            printf("Element found at position %d.", j+1);
+            break;
+        }
+    }
+    if(j==n)
+    printf("element not found\n");
     return 0;
 }
