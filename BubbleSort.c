49ae80b5c55f6146f9550fde3b11379351a06558
azimhossain <azimhossain.ahs@gmail.com>
2025-04-26 09:36:17 +0600

    complete bubble sort

--- BubbleSort.c
+++ BubbleSort.c
@@ -7,11 +7,26 @@ int main()
     if (n < 0)
         return 1;
     int arr[n];
-    printf("Enter %d element : ", n);
+    printf("Enter %d elements : ", n);
     for (int i = 0; i < n; i++)
     {
         scanf("%d", &arr[i]);
     }
 
     // sort the entire array
+    for(int i = 0; i< n-1;i++){
+        for(int j = i;j< n-i-1;j++){
+            if(arr[j] > arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    printf("Sorted Array : ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
 }
